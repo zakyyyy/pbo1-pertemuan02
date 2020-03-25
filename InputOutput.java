3a7a9ea9f236bd51ba6fc1454e66d4f94e77773c
zaky <mzaky2678@gmail.com>
2020-03-25 15:54:20 +0800

    Delete InputOutput.java

--- InputOutput.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class InputOutput{
- public static void main(String [ ] args){
-   Scanner scanner = new Scanner(System.in);
-}   String nama;
-   System.out.print("Masukan nama anda: ");
-   nama = scanner.nexLine();
-
