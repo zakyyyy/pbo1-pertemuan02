56bcd41aa0033de18948d0e4a8036de32d18863f
zaky <mzaky2678@gmail.com>
2020-03-25 15:52:04 +0800

    Buat Input String kedalam Variable

--- InputOutput.java
+++ InputOutput.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class InputOutput{
  public static void main(String [ ] args){
    Scanner scanner = new Scanner(System.in);
-  }
-}
+}   String nama;
+   System.out.print("Masukan nama anda: ");
+   nama = scanner.nexLine();
+
